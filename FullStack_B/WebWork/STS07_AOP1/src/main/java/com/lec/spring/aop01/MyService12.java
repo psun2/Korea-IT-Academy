52b57c9fab0514b190c01301a04db9a2231b3816
psun2 <tjddjs90@naver.com>
2020-11-21 05:42:18 +0900

    2020-11-20 AOP

---
+++ FullStack_B/WebWork/STS07_AOP1/src/main/java/com/lec/spring/aop01/MyService12.java
@@ -0,0 +1,15 @@
+package com.lec.spring.aop01;
+
+import com.lec.spring.beans.Logger;
+import com.lec.spring.beans.Service;
+
+public class MyService12 extends Service {
+    @Override
+    public void doAction() {
+        new Logger().login(); // 공통기능
+
+        System.out.println("MyService11 의 doAction() 호출"); // 핵심기능
+
+        new Logger().logout(); // 공통기능
+    }
+}
---
+++ FullStack_B/WebWork/STS07_AOP1/src/main/java/com/lec/spring/aop03/MyService32.java
@@ -0,0 +1,21 @@
+package com.lec.spring.aop03;
+
+import com.lec.spring.beans.ServiceEx;
+
+public class MyService32 extends ServiceEx {
+
+    @Override
+    public void doWorking() {
+        printInfo();
+    }
+
+    @Override
+    public void quitAction() {
+        printInfo();
+    }
+
+    @Override
+    public void doAction() {
+        printInfo();
+    }
+}
